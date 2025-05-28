48ffe42bff7d86931c1a41ecd5e9140d236ca2f1
ElectronixTM <ElectronixTM@noemail.inkling.su>
2025-05-28 22:33:47 +0300

    feat: починил код и ответил на вопросы
    Изменил макрос в соответствии со своим видением его реализации - убрал
    неявную точку с запятой и поставил потребовал ставить ее явно. Теперь
    бесконечный цикл не образуется

--- test_3/test_3.c
+++ test_3/test_3.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 #ifdef DBG_PRINT
-#define DEBUG_PRINT(x,y,z,i) printf(x,y,z,i);
+#define DEBUG_PRINT(x,y,z,i) printf(x,y,z,i)
 #else
 #define DEBUG_PRINT(x,y,z,i)
 #endif
@@ -25,7 +25,7 @@ int main(int argc, char* argv[])
                                 {"%s %f %d\n", "Item3", M_SQRT1_2, 44444} };
     while (i < (sizeof (z_array) / sizeof (struct z_info))) {
        if (z_array[i].z_int != 0xBADC00DE)
-          DEBUG_PRINT(z_array[i].z_fmt, z_array[i].z_name, z_array[i].z_float, z_array[i].z_int)
+          DEBUG_PRINT(z_array[i].z_fmt, z_array[i].z_name, z_array[i].z_float, z_array[i].z_int);
        i++;
     }
     return 0;
