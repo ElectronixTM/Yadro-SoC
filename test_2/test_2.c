127083e184511e83f20e1a365d11ecfc7d62d947
ElectronixTM <ElectronixTM@noemail.inkling.su>
2025-05-28 20:46:28 +0300

    feat: улучшил читаемость кода

--- test_2/test_2.c
+++ test_2/test_2.c
@@ -6,34 +6,18 @@ int main(int argc, char* argv[])
 {   
   long x[3] = {12345, 54321, 112233};
   printf("Hi, print here some infos!\n");
+  // probably it should overflow... not sure
   printf("Strange: %hd %d %hd\n", x[0], x[1], x[2]);
   printf("But in reality: %ld %ld %ld\n", x[0], x[1], x[2]);
   // hex dump
-  int i; char *p = (char *)&x;
+  char *p = (char *)&x;
   printf("Start hex-dump, addr %lx :\n", (unsigned long) p);
-  for (i = 0; i < sizeof(x); i++)
+  for (size_t i = 0; i < sizeof(x); i++)
   {
-    printf("%02hd ", p[i]);
+    printf("%02x ", p[i]);
   }
   puts("\nI think i figured it out. Data type was wrong");
-  // some various checks
-  long *lp = (long *)&x;
-  if (lp[0] > lp[1])
-  {
-    puts("0 > 1");
-  }
-  else
-  {
-    puts("1 > 0");
-  }
-
-  if (lp[2] > lp[1])
-  {
-    puts("2 > 1");
-  }
-  else
-  {
-    puts("1 > 2");
-  }
+  puts(x[0] > x[1] ? "0 > 1" : "1 > 0");
+  puts(x[1] > x[2] ? "1 > 2" : "2 > 1");
   return 0;
 }
